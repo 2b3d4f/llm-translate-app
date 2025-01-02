208539bba8b154035d7ad1567c3a245a7ed87c4e
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-02 20:24:09 +0900

    fix: remove full height class from main element in Layout component

--- src/renderer/src/layouts/Layout.tsx
+++ src/renderer/src/layouts/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 export default function Layout(): JSX.Element {
   return (
     <>
-      <main className="h-full">
+      <main>
         <Outlet />
       </main>
     </>
