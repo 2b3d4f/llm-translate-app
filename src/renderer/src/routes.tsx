cf2b69e8ac433c8b4c495d79e2b8178579e16838
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-02 19:51:43 +0900

    refactor: remove unused Second route and its component

--- src/renderer/src/routes.tsx
+++ src/renderer/src/routes.tsx
@@ -1,11 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import App from '@renderer/routes/App'
 import Layout from '@renderer/layouts/Layout'
-import Second from '@renderer/routes/Settings'
 
 export const routes = createRoutesFromElements(
   <Route element={<Layout />}>
     <Route path="/" element={<App />}></Route>
-    <Route path="/second" element={<Second />}></Route>
   </Route>
 )
