5f52e0054dc56e695356ba375dff091eeab7237d
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-02 19:52:09 +0900

    refactor: remove unused header and links from Layout component

--- src/renderer/src/layouts/Layout.tsx
+++ src/renderer/src/layouts/Layout.tsx
@@ -1,18 +1,8 @@
-import { Link, Outlet } from 'react-router'
+import { Outlet } from 'react-router'
 
 export default function Layout(): JSX.Element {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <Link to="/">Home page</Link>
-          </li>
-          <li>
-            <Link to="/second">Second page</Link>
-          </li>
-        </ul>
-      </header>
       <main>
         <Outlet />
       </main>
