dc67a69c57ce4f54f820e5a2b72bdab330850bff
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-02 22:09:14 +0900

    feat: add Versions component to About route for displaying version information

--- src/renderer/src/routes/About.tsx
+++ src/renderer/src/routes/About.tsx
@@ -1,9 +1,11 @@
 import Header from '@renderer/components/Header'
+import Versions from '@renderer/components/Versions'
 
 function About(): JSX.Element {
   return (
     <>
       <Header />
+      <Versions />
     </>
   )
 }
