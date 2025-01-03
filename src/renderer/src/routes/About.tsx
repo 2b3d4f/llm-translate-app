2f455cc5a1d030a6a1b59ad7bd425d12060489ee
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-03 17:04:02 +0900

    feat: add debug controls for native theme management in About component

--- src/renderer/src/routes/About.tsx
+++ src/renderer/src/routes/About.tsx
@@ -1,9 +1,44 @@
+import { trpcReact } from '@renderer/trpc'
+
 import Versions from '@renderer/components/Versions'
+import { Alert, AlertDescription, AlertTitle } from '@renderer/components/ui/alert'
+
+import { Button } from '@renderer/components/ui/button'
+import { Sun, Moon, Laptop, Terminal } from 'lucide-react'
 
 function About(): JSX.Element {
+  const setNativeThemeDark = trpcReact.setNativeThemeDark.useMutation()
+  const setNativeThemeLight = trpcReact.setNativeThemeLight.useMutation()
+  const setNativeThemeSystem = trpcReact.setNativeThemeSystem.useMutation()
+
   return (
     <>
       <Versions />
+      <div>
+        <Alert>
+          <Terminal className="h-4 w-4" />
+          <AlertTitle>Debug Controls</AlertTitle>
+          <AlertDescription>
+            The following buttons are for debugging purposes only. Use at your own discretion.
+          </AlertDescription>
+        </Alert>
+        <Button variant={'ghost'} size={'sm'} onClick={() => setNativeThemeLight.mutate()}>
+          <Sun /> Set native theme to Light
+        </Button>
+        <Button variant={'ghost'} size={'sm'} onClick={() => setNativeThemeDark.mutate()}>
+          <Moon /> Set native theme to Dark
+        </Button>
+        <Button variant={'ghost'} size={'sm'} onClick={() => setNativeThemeSystem.mutate()}>
+          <Laptop /> Set native theme to System
+        </Button>
+        <Button
+          variant={'ghost'}
+          size={'sm'}
+          onClick={() => localStorage.removeItem('vite-ui-theme')}
+        >
+          Remove <code>vite-ui-theme</code> from Local Storage
+        </Button>
+      </div>
     </>
   )
 }
