14a915ec99462a8de24948aae7ccfe74dd510ec1
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-05 20:38:22 +0900

    feat: add TitleBarBase component for custom title bar implementation

---
+++ src/renderer/src/components/TitleBarBase.tsx
@@ -0,0 +1,22 @@
+interface TitleBarBaseProps {
+  children: React.ReactNode
+}
+
+function TitleBarBase({ children }: TitleBarBaseProps): JSX.Element {
+  return (
+    <header
+      className="fixed"
+      style={{
+        left: 'env(titlebar-area-x, 0)',
+        top: 'env(titlebar-area-y, 0)',
+        height: 'env(titlebar-area-height, 50px)',
+        width: 'env(titlebar-area-width, 100%)',
+        '-webkit-app-region': 'drag'
+      }}
+    >
+      {children}
+    </header>
+  )
+}
+
+export default TitleBarBase
