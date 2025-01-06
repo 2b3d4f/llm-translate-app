9110ff00fa27b36e3d9e5c00e0860439d2a5ec92
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-06 11:44:15 +0900

    feat: add Help button to TitleBar with accessibility improvements

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,10 +1,16 @@
 import TitleBarBase from './TitleBarBase'
+import { Button } from './ui/button'
+import { HelpCircle } from 'lucide-react'
 
 function TitleBar(): JSX.Element {
   return (
     <TitleBarBase>
-      <div className="h-full flex items-center bg-opacity-0 hover:bg-opacity-50 bg-white px-4">
+      <div className="h-full *:max-h-full flex items-center justify-between bg-opacity-0 hover:bg-opacity-50 bg-white px-4">
         <div className="text-[12px] text-black">Electron</div>
+        <Button variant={'ghost'} size={'icon'} className="rounded-none">
+          <span className="sr-only">About</span>
+          <HelpCircle absoluteStrokeWidth strokeWidth={1.5} />
+        </Button>
       </div>
     </TitleBarBase>
   )
