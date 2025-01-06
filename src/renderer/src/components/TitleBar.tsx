bfef6b9ee19b1897319e2cb5bf9e7b8a241afc87
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-06 11:50:47 +0900

    feat: integrate Tooltip for Help button in TitleBar with accessibility enhancements

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,4 +1,5 @@
 import TitleBarBase from './TitleBarBase'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
 import { Button } from './ui/button'
 import { HelpCircle } from 'lucide-react'
 
@@ -7,10 +8,19 @@ function TitleBar(): JSX.Element {
     <TitleBarBase>
       <div className="h-full *:max-h-full flex items-center justify-between bg-opacity-0 hover:bg-opacity-50 bg-white px-4">
         <div className="text-[12px] text-black">Electron</div>
-        <Button variant={'ghost'} size={'icon'} className="rounded-none">
-          <span className="sr-only">About</span>
-          <HelpCircle absoluteStrokeWidth strokeWidth={1.5} />
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant={'ghost'} size={'icon'} className="rounded-none w-[44px]">
+                <span className="sr-only">About</span>
+                <HelpCircle absoluteStrokeWidth strokeWidth={1.5} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>About</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </TitleBarBase>
   )
