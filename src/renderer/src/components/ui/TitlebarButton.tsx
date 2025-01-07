0f51e0ab2dfa7871c6e643d065ba965f1aee6217
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-07 11:04:20 +0900

    feat: refactor TitlebarButton to use forwardRef and improve class handling

--- src/renderer/src/components/ui/TitlebarButton.tsx
+++ src/renderer/src/components/ui/TitlebarButton.tsx
@@ -1,42 +1,41 @@
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip'
 import { Button } from './button'
-import { ReactNode } from 'react'
-import { cn } from '../../lib/utils'
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { forwardRef } from 'react'
 
 interface TitlebarButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode
+  children: React.ReactNode
   tooltip: string
   width?: string
   onClick?: () => void
 }
 
-function TitlebarButton({
-  children,
-  tooltip,
-  width = '44px',
-  onClick,
-  ...props
-}: TitlebarButtonProps): JSX.Element {
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant={'ghost'}
-            size={'icon'}
-            className={cn('rounded-none', `w-[${width}]`)}
-            onClick={onClick}
-            {...props}
-          >
-            {children}
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent className="bg-background text-foreground border border-input shadow-sm">
-          <p>{tooltip}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  )
-}
+const TitlebarButton = forwardRef<HTMLButtonElement, TitlebarButtonProps>(
+  ({ children, tooltip, width = '44px', onClick, ...props }, ref) => {
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              ref={ref}
+              variant={'ghost'}
+              className={twMerge(clsx(`w-[${width}]`, 'h-full', 'rounded-none'))}
+              onClick={onClick}
+              {...props}
+            >
+              {children}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent className="bg-background text-foreground border border-input shadow-sm">
+            <p>{tooltip}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    )
+  }
+)
+
+TitlebarButton.displayName = 'TitlebarButton'
 
 export { TitlebarButton }
