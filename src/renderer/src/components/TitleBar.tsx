0fc1a099e79888e29c0294716a6ab35dc911f294
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-07 11:02:17 +0900

    feat: add AboutDialog component and integrate it into TitleBar

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,15 +1,18 @@
 import { TitlebarBase } from './TitlebarBase'
 import { TitlebarButton } from './ui/TitlebarButton'
 import { HelpCircle } from 'lucide-react'
+import { AboutDialog } from './AboutDialog'
 
 function Titlebar(): JSX.Element {
   return (
     <TitlebarBase>
       <div className="h-full *:max-h-full flex items-center justify-between bg-opacity-0 hover:bg-opacity-50 px-4">
         <div className="text-[12px]">Electron</div>
-        <TitlebarButton tooltip="About">
-          <HelpCircle absoluteStrokeWidth strokeWidth={1.5} />
-        </TitlebarButton>
+        <AboutDialog>
+          <TitlebarButton tooltip="About">
+            <HelpCircle absoluteStrokeWidth strokeWidth={1.5} />
+          </TitlebarButton>
+        </AboutDialog>
       </div>
     </TitlebarBase>
   )
