6b64e5ab7e7331ef91ae9cea02620804984de976
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-01-07 17:11:54 +0900

    feat: add ping functionality to AboutDialog with TRPC integration

--- src/renderer/src/components/AboutDialog.tsx
+++ src/renderer/src/components/AboutDialog.tsx
@@ -7,15 +7,28 @@ import {
   DialogTrigger
 } from './ui/dialog'
 import { Suspense } from 'react'
+import { Button } from './ui/button'
 
 import { ModeToggle } from './mode-toggle'
-import { SunMoon } from 'lucide-react'
+import { SunMoon, Terminal } from 'lucide-react'
+
+import { trpcReact } from '../trpc'
 
 interface AboutDialogProps {
   children: React.ReactNode
 }
 
 function AboutDialog({ children }: AboutDialogProps): JSX.Element {
+  const { mutate: ping } = trpcReact.ping.useMutation({
+    onSuccess: (data) => {
+      console.log(data)
+    }
+  })
+
+  const handlePing = (): void => {
+    ping()
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -34,6 +47,16 @@ function AboutDialog({ children }: AboutDialogProps): JSX.Element {
               </p>
             </div>
             <ModeToggle />
+          </div>
+          <div className=" flex items-center space-x-4 rounded-md border p-4">
+            <Terminal />
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">Ping</p>
+              <p className="text-sm text-muted-foreground">Send ping to the main process.</p>
+            </div>
+            <Button onClick={handlePing} variant={'outline'}>
+              Ping
+            </Button>
           </div>
         </DialogContent>
       </Suspense>
